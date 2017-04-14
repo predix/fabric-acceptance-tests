74ec35aa77b074d2f679df07cb9a81b79a6429dd
Atul Kshirsagar <atul.kshirsagar@ge.com>
2017-04-13 18:38:31 -0700

    Add tests for register user, deploy chaincode and query chaincode

--- specs/blockchain.spec.js
+++ specs/blockchain.spec.js
@@ -1,27 +1,106 @@
 var expect = require('expect.js');
-var hyperledgerUtil = require('./utils/hyperledgerUtil.js');
-var config = require('./config/config.js');
+var hyperledgerUtil = require('./utils/hyperledgerUtil');
+var config = require('./config/config');
 var Promise = require('bluebird');
+var mocha = require('mocha')
+var coMocha = require('co-mocha')
 
-describe('Blockchain suite', function() {
-    before(function() {
+coMocha(mocha)
+
+describe('Blockchain', function() {
+    var chaincodeID;
+
+    before(function*() {
         expect(config.membershipAddress).not.to.be(undefined);
         expect(config.membershipAddress).not.to.be.empty();
         expect(config.peers).not.to.be(undefined);
         expect(config.peers).not.to.be.empty();
-        hyperledgerUtil.setupChain('predixBlockchain', config.membershipAddress, config.peers);
+        hyperledgerUtil.setupChain("predixBlockchain", config.membershipAddress, config.peers, "/Users/geoss/tmp/keyValStore");
     })
 
-    it('should be able to enroll registrar', function(done) {
+    it('should be able to enroll registrar', function*() {
         console.log("Starting enrolling user");
-        var enrollUser = Promise.promisify(hyperledgerUtil.enrollUser)
-        enrollUser("someuser", "doesntmatter").then(function() {
-        	console.log("Successfully enrolled----")
-            done()
-        }).catch(function(err) {
-        	console.log("Error in enrolling user ", err)
+        try {
+            yield hyperledgerUtil.enrollRegistrar("WebAppAdmin1", "DJY27pEnl16d");
+            console.log("Done enrolling registrar");
+            var registrar = hyperledgerUtil.getRegistrar();
+            expect(registrar.getName()).to.be('WebAppAdmin1');
+        } catch (err) {
             expect().fail(err.message);
-            done();
-        })
+        }
     });
+
+    it('should be able to enroll a regular user', function*() {
+        console.log("Starting enrolling user");
+        try {
+            yield hyperledgerUtil.enrollUser("system_chaincode_invoker", "DRJ20pEql15a");
+        } catch (err) {
+            expect().fail(err.message);
+        }
+    });
+
+    it('should register and enroll a new user', function*() {
+        console.log("Starting registering new user");
+        try {
+            var name = "newuser";
+            var enrollmentSecret = yield hyperledgerUtil.registerUser(name);
+            console.log("Enrollment secret for", name, "is", enrollmentSecret);
+            yield hyperledgerUtil.enrollUser(name, enrollmentSecret);
+            console.log("Successfully registered and enrolled a new user", name);
+        } catch (err) {
+            expect().fail(err.message);
+        }
+    });
+
+    it('should be able to deploy chaincode', function*(done) {
+        this.timeout(30000);
+        console.log("Starting deploying chaincode");
+        try {
+            var chaincodePath = "github.com/predix/chaincode_example/chaincode_example01";
+            var args = ["a", "500", "b", "100"];
+            var user = yield hyperledgerUtil.getUser("newuser");
+            var tx = yield hyperledgerUtil.deployChaincode(user, args, chaincodePath);
+            tx.on('complete', function(results) {
+                // Deploy request completed successfully
+                console.log("deploy results", results);
+                // Set the testChaincodeID for subsequent tests
+                chaincodeID = results.chaincodeID;
+                console.log("Successfully deployed chaincode", chaincodeID)
+                done();
+            });
+            tx.on('error', function(err) {
+                console.log("Failed to deploy chaincode", err)
+                done(err);
+            });
+        } catch (err) {
+            expect().fail(err.message);
+        }
+    })
+
+    it.only('should be able to query chaincode', function*(done) {
+        this.timeout(30000);
+        console.log("Starting querying chaincode");
+        try {
+            var chaincodeID = "3be65a2f291efd2f8d1f1b3d7bda5022e25689313840b1b465a7ff304c452d34c825d102c8a828665791140dd29213b79851de82cd6d21e78b8dad3bbb32ca8e";
+            var args = ["c"];
+            var user = yield hyperledgerUtil.getUser("newuser");
+            var tx = yield hyperledgerUtil.queryChaincode(user, args, chaincodeID);
+            tx.on('complete', function(results) {
+                // Deploy request completed successfully
+                console.log("query results", results.result.toString());
+                // Set the testChaincodeID for subsequent tests
+                // chaincodeID = results.chaincodeID;
+                // console.log("Successfully deployed chaincode", chaincodeID)
+                done();
+            });
+            tx.on('error', function(err) {
+                console.log("Failed to query chaincode", err)
+                expect().fail(err.message);
+                // done(err);
+            });
+        } catch (err) {
+            expect().fail(err.message);
+        }
+    })
+
 });
--- specs/utils/hyperledgerUtil.js
+++ specs/utils/hyperledgerUtil.js
@@ -1,52 +1,110 @@
-var Promise = require('bluebird');
-// var request = require('request')
+var bluebird = require('bluebird');
 var hfc = require('hfc');
 var fs = require('fs');
 
-var hyperledgerUtil = function() {
-    var chain;
+var chain;
 
-    this.setupChain = function(chainName, caAddr, peers) {
-        // Name could be the service instance name that we get from env
-        chain = hfc.newChain(chainName);
-        console.log("Adding membership service ", caAddr);
-        chain.setMemberServicesUrl("grpc://" + caAddr);
-        peers.split(",").forEach(peer => {
-            console.log("Adding peer ", peer);
-            chain.addPeer("grpc://" + peer);
-        });
-        chain.setKeyValStore(hfc.newFileKeyValStore("/Users/geoss/tmp/keyValStore"));
-        console.log("Setup the keyval store");
-    }
-
-    this.enrollUser = function(name, passwd) {
-        chain.getMember(name, function(err, client) {
-            if (err) {
-                console.log("Failed to get user ", name);
-                throw new Error(err.message);
-            } else {
-                console.log("Successfully got the user ", name);
-                client.enroll(passwd, function(err, crypto) {
-                    if (err) {
-                        console.log("Failed to enroll user ", name);
-                        throw new Error(err.message);
-                    } else {
-                        console.log("Successfully enrolled user ", name);
-                        var path = chain.getKeyValStore().dir + "/member." + WebAppAdmin.getName();
-                        fs.exists(path, function(exists) {
-                            if (exists) {
-                                console.log("Successfully stored client token");
-                            } else {
-                                console.log("Failed to store client token for " + client.getName() + ", error: " + err);
-                                throw new Error(err.message);
-                            }
-                        });
-                    }
-                });
-                console.log("Done enrolling")
-            }
-        });
-    }
+function* setupChain(chainName, caAddr, peers, keystoreLocation) {
+    chain = hfc.newChain(chainName);
+    console.log("Adding membership service ", caAddr);
+    chain.setMemberServicesUrl("grpc://" + caAddr);
+    peers.split(",").forEach(peer => {
+        console.log("Adding peer ", peer);
+        chain.addPeer("grpc://" + peer);
+    });
+    chain.setKeyValStore(hfc.newFileKeyValStore(keystoreLocation));
+    console.log("Setup the keyval store");
 }
 
-module.exports = new hyperledgerUtil();
+function* getUser(name) {
+    var getUser = bluebird.promisify(chain.getUser, {
+        context: chain
+    });
+    var user = yield getUser(name);
+    return user;
+}
+
+function* enrollRegistrar(name, passwd) {
+    console.log("Entering enrollRegistrar");
+    var user = yield * enrollUser(name, passwd);
+    chain.setRegistrar(user);
+    console.log("Successfully enrolled registrar");
+    return user;
+}
+
+function* enrollUser(name, passwd) {
+    console.log("Entering enrollUser");
+    var client = yield * getUser(name);
+    console.log("Successfully got the user ", name);
+    if (client.isEnrolled()) {
+        console.log("Client", client.getName(), "is already enrolled");
+        return client;
+    }
+    var enroll = bluebird.promisify(client.enroll, {
+        context: client
+    });
+    yield enroll(passwd);
+    console.log("Successfully enrolled user", name);
+    var path = chain.getKeyValStore().dir + "/member." + client.getName();
+    var exists = fs.existsSync(path);
+    if (!exists) {
+        console.log("Failed to store client certs for", client.getName(), ", error:", err);
+        throw new Error("Failed to store client token")
+    }
+    console.log("Successfully stored client certs");
+    return client
+}
+
+function getRegistrar() {
+    return chain.getRegistrar();
+}
+
+function* registerUser(name) {
+    console.log("Entering registerUser");
+    var user = yield * getUser(name);
+    var registerUsr = bluebird.promisify(user.register, {
+        context: user
+    });
+    var registrationRequest = {
+        enrollmentID: name,
+        affiliation: "institution_a"
+    };
+    var secret = yield registerUsr(registrationRequest);
+    console.log("Registered user", name);
+    return secret;
+}
+
+function* deployChaincode(user, args, chaincodePath) {
+    console.log("Entering deployChaincode");
+    var deployRequest = {
+        chaincodePath: chaincodePath,
+        fcn: "init",
+        args: args
+    }
+    var deployTx = user.deploy(deployRequest);
+    console.log("Submitted deploy transaction");
+    return deployTx;
+}
+
+function* queryChaincode(user, args, chaincodeID) {
+    console.log("Entering queryChaincode");
+    var queryRequest = {
+        chaincodeID: chaincodeID,
+        fcn: "query",
+        args: args
+    }
+    var queryTx = user.query(queryRequest);
+    console.log("Submitted query transaction");
+    return queryTx;
+}
+
+module.exports = {
+    getRegistrar: getRegistrar,
+    getUser: bluebird.coroutine(getUser),
+    setupChain: bluebird.coroutine(setupChain),
+    enrollUser: bluebird.coroutine(enrollUser),
+    enrollRegistrar: bluebird.coroutine(enrollRegistrar),
+    registerUser: bluebird.coroutine(registerUser),
+    deployChaincode: bluebird.coroutine(deployChaincode),
+    queryChaincode: bluebird.coroutine(queryChaincode)
+}
